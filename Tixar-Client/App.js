dc7e823fc938705f991449b65b6c50c126436aff
Genekkion <115409114+Genekkion@users.noreply.github.com>
2023-09-04 18:13:39 +0800

    Updated app.js for testing pages

--- Tixar-Client/App.js
+++ Tixar-Client/App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ForgetPasswordPage from './pages/login/forgetPassword';
+import SetPasswordPage from './pages/login/setPassword';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        'Lato-Bold': require('./assets/fonts/Lato/Lato-Bold.ttf'),
+        'Lato-Regular': require('./assets/fonts/Lato/Lato-Regular.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        console.log('font not loaded');
+        return;
+    }
+    console.log('fonts loaded');
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <ForgetPasswordPage 
+            onLayout={onLayoutRootView} />
+
     );
 }
 
@@ -17,5 +38,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-    },
+    }, text: {
+        fontFamily: 'Lato-Bold',
+    }
 });
