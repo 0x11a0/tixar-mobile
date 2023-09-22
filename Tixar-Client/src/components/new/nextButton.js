7465eb606f8ad573213bbb3491fe8b4e34c5c29c
Wei Jie <limweijie63@gmail.com>
2023-09-23 03:05:28 +0800

    feat/added create fanclub button/weijie

---
+++ Tixar-Client/src/components/new/nextButton.js
@@ -0,0 +1,43 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { Pressable, Text, StyleSheet } from "react-native";
+
+export default NextButton = ({ buttonText, onPressFunction, buttonHeight }) => {
+    return (
+        <LinearGradient colors={['#FF0080', '#7928CA']}
+            style={{
+                borderRadius: 8,
+                width: '90%',
+                height: buttonHeight ? buttonHeight : 40,
+                alignItems: 'center',
+                justifyContent: 'center',
+                alignSelf: 'center',
+            }}
+            start={[0, 0]} end={[1, 0]}>
+
+            <Pressable style={styles.button}
+                onPress={() => { onPressFunction(); }} >
+
+                <Text style={styles.buttonText}>
+                    {buttonText}
+                </Text>
+
+            </Pressable>
+
+        </LinearGradient >
+    )
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+        width: '90%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 15,
+        fontFamily: 'Lato-Bold',
+        color: 'white',
+    }
+});
--- Tixar-Client/src/screens/new/adminDashboard.js
+++ Tixar-Client/src/screens/new/adminDashboard.js
@@ -1,33 +1,49 @@
-import { React, useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Pressable } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, Pressable, ScrollView } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import FanclubCard from '../../components/new/fanclubCard'; // Note the capital "F"
+import FanclubCard from '../../components/new/fanclubCard';
+import NextButton from '../../components/new/nextButton';
 
 export default AdminDashboard = ({ navigation }) => { 
 
     return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#F2F2F2' }}>
+            <View style={styles.container}>
 
-        <View style={styles.cardContainer}>
+                <ScrollView>
+                    {/* Your FanclubCards go here */}
+                    <FanclubCard 
+                        navigationDestination={'NotificationsPage'} //place holder page, change to fanclub page
+                        clubName={'Taylor Swift'}
+                        fanNumber={'1000'}
+                        codesActive={'100'}
+                    />
 
-            <FanclubCard 
-                navigationDestination={'NotificationsPage'}
-                clubName={'Taylor Swift'}
-                fanNumber={'1000'}
-                codesActive={'100'}
-            />
+                    {/* Add more FanclubCards as needed */}
+                </ScrollView>
 
+                {/* Next Button */}
+                <View style={styles.buttonContainer}>
+                    <NextButton
+                        buttonText={'Create New Fanclub'}
+                        onPressFunction={() => navigation.navigate('NotificationsPage')} //place holder destination, change to create new fanclub page
+                        buttonHeight={50}
+                    />
+                </View>
+
+            </View>
             
-        </View>
- 
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-
-    cardContainer: {
-        flex: 1, //grow and take up the space of the parent container
-        justifyContent: 'flex-start',
-        flexDirection: 'column',
-        // backgroundColor: 'red',
-    }
+    container: {
+        flex: 1,
+    },
+    buttonContainer: {
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        padding: 16,
+    },
 });
