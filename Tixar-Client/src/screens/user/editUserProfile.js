47967b514870763a2ebf7104e22b68c75ae00b55
kloe <ongweitingchloe@gmail.com>
2023-09-24 21:07:26 +0800

    bug/Edited user profile to run everytime the page loads/chloe

--- Tixar-Client/src/screens/user/editUserProfile.js
+++ Tixar-Client/src/screens/user/editUserProfile.js
@@ -1,255 +1,249 @@
-import { React, useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Image, Pressable, TextInput, FlatList, TouchableOpacity, Button, onPressLearnMore, Alert } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-
+import { React, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Pressable,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Button,
+  onPressLearnMore,
+  Alert,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default EditUserProfile = ({ route, navigation }) => {
-    const [firstNameField, setFirstNameField] = useState('');
-    const handleFirstName = (text) => {
-        setFirstNameField(text);
-    };
+  const [firstNameField, setFirstNameField] = useState("");
+  const handleFirstName = (text) => {
+    setFirstNameField(text);
+  };
 
-    const [lastNameField, setLastNameField] = useState('');
-    const handleLastName = (text) => {
-        setLastNameField(text);
-    };
+  const [lastNameField, setLastNameField] = useState("");
+  const handleLastName = (text) => {
+    setLastNameField(text);
+  };
 
-    let { firstName, lastName, phoneNumber, email, token } = route.params;
+  let { firstName, lastName, phoneNumber, email, token } = route.params;
 
-    const [emailField, setEmailField] = useState('');
-    const handleEmail = (text) => {
-        setEmailField(text);
-    };
+  const [emailField, setEmailField] = useState("");
+  const handleEmail = (text) => {
+    setEmailField(text);
+  };
 
-    const [numberField, setNumberField] = useState('');
-    const handleNumber = (text) => {
-        setNumberField(text);
-    };
+  const handleUpdate = () => {
+    fetch("http://rt.tixar.sg/api/user", {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: route.params.token,
+      },
+      body: JSON.stringify({
+        firstName: firstNameField === "" ? firstName : firstNameField,
+        lastName: lastNameField === "" ? lastName : lastNameField,
+        email: emailField === "" ? email : emailField,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        console.log("Update successful");
+      } else {
+        console.log("Update unsuccessful");
+      }
+    });
+  };
 
-    const handleUpdate = () => {
-        fetch('http://rt.tixar.sg/api/user', {
-            method: 'PUT',
-            credentials: 'include',
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': route.params.token
-            },
-            body: JSON.stringify({
-                "firstName": firstNameField === '' ? firstName : firstNameField,
-                "lastName": lastNameField === '' ? lastName : lastNameField,
-                "phone": numberField === '' ? phoneNumber : numberField,
-                "email": emailField === '' ? email : emailField,
-            }),
-        }).then((response) => {
-            if (response.ok) {
-                console.log("Update successful");
-            } else {
-                console.log("Update unsuccessful")
-            }
-        });
-    }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerBox}>
+        <Image
+          source={require("../../assets/soft-ui-pro-react-native-v1.1.1/background3x.png")}
+          style={styles.headerImage}
+        />
+        <Text style={styles.title}>TIXAR</Text>
+      </View>
 
-    return (
-        <SafeAreaView style={styles.container}>
-
-            <View style={styles.headerBox}>
-                <Image source={require('../../assets/soft-ui-pro-react-native-v1.1.1/background3x.png')}
-                    style={styles.headerImage} />
-                <Text style={styles.title}>TIXAR</Text>
-            </View>
-
-            <View style={styles.translucentBox}>
-                <Text style={styles.subtitle}>Edit Profile</Text>
-                <Image source={require('../../assets/profilepicture.png')} style={styles.profileImage} />
-                <View style={styles.fieldBox}>
-                    <TextInput
-                        style={styles.fieldText}
-                        onChangeText={handleFirstName}
-                        value={firstNameField}
-                        placeholder="First name"
-                    />
-                </View>
-                <View style={styles.fieldBox}>
-                    <TextInput
-                        style={styles.fieldText}
-                        onChangeText={handleLastName}
-                        value={lastNameField}
-                        placeholder="Last name"
-                    />
-                </View>
-                {/* <View style={styles.fieldBox}>
-                    <TextInput
-                        style={styles.fieldText}
-                        onChangeText={handleName}
-                        value={nameField}
-                        placeholder="First name"
-                    />
-                </View> */}
-                <View style={styles.fieldBox}>
-                    <TextInput
-                        style={styles.fieldText}
-                        onChangeText={handleEmail}
-                        value={emailField}
-                        placeholder="Email"
-                    />
-                </View>
-                <View style={styles.fieldBox}>
-                    <TextInput
-                        style={styles.fieldText}
-                        onChangeText={handleNumber}
-                        value={numberField}
-                        placeholder="Phone Number (Optional)"
-                    />
-                </View>
-                <ResetButton
-                    isValid={firstNameField !== '' || lastNameField !== '' || emailField !== '' || numberField !== ''}
-                    // isValid={nameField !== name || emailField !== email || numberField !== phoneNumber}
-                    onPressFunction={() => {
-                        handleUpdate();
-                        navigation.pop();
-                    }}
-                />
-
-            </View>
-            <Text style={styles.footerText}>TIXAR</Text>
-
-        </SafeAreaView >
-    )
-
-}
+      <View style={styles.translucentBox}>
+        <Text style={styles.subtitle}>Edit Profile</Text>
+        <Image
+          source={require("../../assets/profilepicture.png")}
+          style={styles.profileImage}
+        />
+        <View style={styles.fieldBox}>
+          <TextInput
+            style={styles.fieldText}
+            onChangeText={handleFirstName}
+            value={firstNameField}
+            placeholder="First name"
+          />
+        </View>
+        <View style={styles.fieldBox}>
+          <TextInput
+            style={styles.fieldText}
+            onChangeText={handleLastName}
+            value={lastNameField}
+            placeholder="Last name"
+          />
+        </View>
+        <View style={styles.fieldBox}>
+          <TextInput
+            style={styles.fieldText}
+            onChangeText={handleEmail}
+            value={emailField}
+            placeholder="Email"
+          />
+        </View>
+        <ResetButton
+          isValid={
+            firstNameField !== "" || lastNameField !== "" || emailField !== ""
+          }
+          onPressFunction={() => {
+            handleUpdate();
+            navigation.pop();
+          }}
+        />
+      </View>
+      <Text style={styles.footerText}>TIXAR</Text>
+    </SafeAreaView>
+  );
+};
 
 const ResetButton = ({ isValid, onPressFunction }) => {
-    return (
-        <LinearGradient colors={isValid ?
-            ['#FF0080', '#7928CA']
-            : ['#E8ECEF', '#E8ECEF']}
-            style={styles.resetBackgroundEnabled}
-            start={[0, 0]} end={[1, 0]}>
-            <Pressable style={styles.resetButton}
-                onPress={() => {
-                    onPressFunction();
-                }} >
-                <Text style={isValid ?
-                    styles.resetTextEnabled
-                    : styles.resetTextDisabled}>
-                    Update Profile</Text>
-            </Pressable>
-        </LinearGradient >
-    )
-}
+  return (
+    <LinearGradient
+      colors={isValid ? ["#FF0080", "#7928CA"] : ["#E8ECEF", "#E8ECEF"]}
+      style={styles.resetBackgroundEnabled}
+      start={[0, 0]}
+      end={[1, 0]}
+    >
+      <Pressable
+        style={styles.resetButton}
+        onPress={() => {
+          onPressFunction();
+        }}
+      >
+        <Text
+          style={isValid ? styles.resetTextEnabled : styles.resetTextDisabled}
+        >
+          Update Profile
+        </Text>
+      </Pressable>
+    </LinearGradient>
+  );
+};
 const styles = StyleSheet.create({
-    headerBox: {
-        position: 'relative',
-        flex: 0.5,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        zIndex: 1,
-    },
-    title: {
-        fontSize: 35,
-        fontFamily: 'Lato-Bold',
-        color: 'white',
-        marginTop: 40,
-    },
-    headerImage: {
-        marginTop: 30,
-        width: '95%',
-        height: 280,
-        borderRadius: 22,
-        resizeMode: 'cover',
-        position: 'absolute',
-    },
+  headerBox: {
+    position: "relative",
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    zIndex: 1,
+  },
+  title: {
+    fontSize: 35,
+    fontFamily: "Lato-Bold",
+    color: "white",
+    marginTop: 40,
+  },
+  headerImage: {
+    marginTop: 30,
+    width: "95%",
+    height: 280,
+    borderRadius: 22,
+    resizeMode: "cover",
+    position: "absolute",
+  },
 
-    profileImage: {
-        flex: 0.5,
-        width: 100,
-        height: 100,
-        resizeMode: 'cover',
-        marginTop: 8,
-    },
+  profileImage: {
+    flex: 0.5,
+    width: 100,
+    height: 100,
+    resizeMode: "cover",
+    marginTop: 8,
+  },
 
-    translucentBox: {
-        height: '75%',
-        width: '85%',
-        position: 'absolute',
-        top: 130,
-        backgroundColor: 'rgba(255, 255, 255, 0.95)',
-        borderRadius: 15,
-        zIndex: 2,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        alignSelf: 'center'
-    },
-    subtitle: {
-        fontSize: 20,
-        fontFamily: 'Lato-Bold',
-        color: '#394051',
-        marginTop: 24,
-    },
+  translucentBox: {
+    height: "75%",
+    width: "85%",
+    position: "absolute",
+    top: 130,
+    backgroundColor: "rgba(255, 255, 255, 0.95)",
+    borderRadius: 15,
+    zIndex: 2,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    alignSelf: "center",
+  },
+  subtitle: {
+    fontSize: 20,
+    fontFamily: "Lato-Bold",
+    color: "#394051",
+    marginTop: 24,
+  },
 
-    container: {
-        flex: 1,
-        backgroundColor: '#f2f2f2',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        borderColor: '#f2f2f2',
-    },
-    footerText: {
-        bottom: 15,
-        fontFamily: 'Lato-Regular',
-        fontSize: 12,
-        position: 'absolute'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderColor: "#f2f2f2",
+  },
+  footerText: {
+    bottom: 15,
+    fontFamily: "Lato-Regular",
+    fontSize: 12,
+    position: "absolute",
+  },
 
-    fieldBox: {
-        height: 56,
-        width: '86%',
-        borderRadius: 10,
-        borderColor: '#D2D6DA',
-        borderWidth: 1,
-        justifyContent: 'center',
-        marginTop: 10,
-    },
-    fieldText: {
-        left: '5%',
-        fontSize: 18,
-        fontFamily: 'Lato-Regular',
-        color: '#8F8F8F',
-    },
+  fieldBox: {
+    height: 56,
+    width: "86%",
+    borderRadius: 10,
+    borderColor: "#D2D6DA",
+    borderWidth: 1,
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  fieldText: {
+    left: "5%",
+    fontSize: 18,
+    fontFamily: "Lato-Regular",
+    color: "#8F8F8F",
+  },
 
-    resetButton: {
-        width: '86%',
-        height: 35,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    resetBackgroundEnabled: {
-        marginTop: 55,
-        borderRadius: 8,
-        width: '86%',
-        height: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    resetBackgroundDisabled: {
-        marginTop: 55,
-        borderRadius: 8,
-        width: '86%',
-        height: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#E8ECEF',
-    },
-    resetTextEnabled: {
-        fontSize: 15,
-        fontFamily: 'Lato-Bold',
-        color: 'white',
-    },
-    resetTextDisabled: {
-        fontSize: 15,
-        fontFamily: 'Lato-Bold',
-        color: '#252F40',
-    },
-
-
+  resetButton: {
+    width: "86%",
+    height: 35,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  resetBackgroundEnabled: {
+    marginTop: 55,
+    borderRadius: 8,
+    width: "86%",
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  resetBackgroundDisabled: {
+    marginTop: 55,
+    borderRadius: 8,
+    width: "86%",
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#E8ECEF",
+  },
+  resetTextEnabled: {
+    fontSize: 15,
+    fontFamily: "Lato-Bold",
+    color: "white",
+  },
+  resetTextDisabled: {
+    fontSize: 15,
+    fontFamily: "Lato-Bold",
+    color: "#252F40",
+  },
 });
--- Tixar-Client/src/screens/user/userprofile.js
+++ Tixar-Client/src/screens/user/userprofile.js
@@ -1,145 +1,168 @@
-import { React, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Image, Pressable, TextInput, FlatList, TouchableOpacity, Button, onPressLearnMore, Alert } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import HeaderBlock from './headerBlockUserProfile';
-const userEdit = ['Ewallet', 'Edit', 'Settings']
+import { React, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Pressable,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  Button,
+  onPressLearnMore,
+  Alert,
+} from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import HeaderBlock from "./headerBlockUserProfile";
+import { useIsFocused } from "@react-navigation/native"; // Import useIsFocused
+
+const userEdit = ["Ewallet", "Edit", "Settings"];
 
 export default UserProfile = ({ route, navigation }) => {
-    let [firstName, setFirstName] = useState('');
-    let [lastName, setLastName] = useState('');
-    let [email, setEmail] = useState('');
-    let [phoneNumber, setPhoneNumber] = useState('');
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [email, setEmail] = useState("");
+  let [phoneNumber, setPhoneNumber] = useState("");
 
+  const isFocused = useIsFocused(); // Use useIsFocused to check if the screen is focused
 
-    const parsePhoneNumber = (phoneNum) => {
-        // convert phone number to formatter phone number
+  const parsePhoneNumber = (phoneNum) => {
+    // convert phone number to formatter phone number
+  };
+
+  const getUser = () => {
+    console.log("GET REQUEST");
+    fetch("http://rt.tixar.sg/api/user", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Authorization: route.params.token,
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setFirstName(data.firstName);
+        setLastName(data.lastName);
+        setEmail(data.email);
+        setPhoneNumber(data.phone);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      getUser(); // Fetch data when the screen is focused
     }
+  }, [isFocused]); // Use isFocused as a dependency in useEffect
 
-    const getUser = () => {
-        fetch('http://rt.tixar.sg/api/user', {
-            method: 'GET',
-            credentials: 'include',
-            headers: { 'Authorization': route.params.token }
-        })
-            .then(response => response.json())
-            .then((data) => {
-                setFirstName(data.firstName);
-                setLastName(data.lastName);
-                setEmail(data.email);
-                setPhoneNumber(data.phone);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    };
+  return (
+    <SafeAreaView style={styles.container}>
+      <HeaderBlock
+        name={firstName + " " + lastName}
+        walletOnPress={() => {
+          navigation.navigate("manageEWalletPage", {
+            token: route.params.token,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phoneNumber: phoneNumber,
+          });
+        }}
+        editOnPress={() => {
+          navigation.navigate("editUserProfilePage", {
+            token: route.params.token,
+          });
+        }}
+        settingsOnPress={() => {
+          navigation.navigate("settingsPage", { token: route.params.token });
+        }}
+      />
 
-    useEffect(() => {
-        getUser();
-    }, []);
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <HeaderBlock name={firstName + ' ' + lastName}
-                walletOnPress={() => {
-                    navigation.navigate('manageEWalletPage',
-                        {
-                            token: route.params.token,
-                            firstName: firstName,
-                            lastName: lastName,
-                            email: email,
-                            phoneNumber: phoneNumber
-                        });
-                }}
-                editOnPress={() => {
-                    navigation.navigate('editUserProfilePage', { token: route.params.token });
-                }}
-                settingsOnPress={() => {
-                    navigation.navigate('settingsPage', { token: route.params.token });
-                }}
+      <View style={styles.translucentBox}>
+        <Text style={styles.email}>Email</Text>
+        <Text style={styles.subtitle}>{email}</Text>
+        <Text style={styles.text}>Phone Number</Text>
+        <Text style={styles.subtitle}>{phoneNumber}</Text>
+        <TouchableOpacity style={{ marginTop: 50 }}>
+          <View style={buttonContainerStyle}>
+            <Button
+              title="View My Tickets"
+              accessibilityLabel="View Tickets"
+              color={Platform.OS === "ios" ? "white" : "#AB2FCD"}
+              onPress={() => {
+                // Alert.alert('Link to view tickets');
+                navigation.navigate("userTicketsPage");
+              }}
             />
+          </View>
+        </TouchableOpacity>
+      </View>
 
-            <View style={styles.translucentBox}>
-                <Text style={styles.email}>Email</Text>
-                <Text style={styles.subtitle}>{email}</Text>
-                <Text style={styles.text}>Phone Number</Text>
-                <Text style={styles.subtitle}>{phoneNumber}</Text>
-                <TouchableOpacity style={{ marginTop: 50 }}>
-                    <View style={buttonContainerStyle}>
-                        <Button title="View My Tickets" accessibilityLabel="View Tickets"
-                            color={Platform.OS === 'ios' ? "white" : "#AB2FCD"}
-                            onPress={() => {
-                                // Alert.alert('Link to view tickets');
-                                navigation.navigate('userTicketsPage');
-                            }} />
-                    </View>
-                </TouchableOpacity>
-            </View>
-
-            <Text style={styles.footerText}>TIXAR</Text>
-        </SafeAreaView>
-    )
-
-}
-const buttonContainerStyle = Platform.OS === 'ios' ? { backgroundColor: "#AB2FCD" } : { backgroundColor: 'transparent' };
+      <Text style={styles.footerText}>TIXAR</Text>
+    </SafeAreaView>
+  );
+};
+const buttonContainerStyle =
+  Platform.OS === "ios"
+    ? { backgroundColor: "#AB2FCD" }
+    : { backgroundColor: "transparent" };
 
 const styles = StyleSheet.create({
+  translucentBox: {
+    height: "50%",
+    width: "85%",
+    position: "absolute",
+    top: 320,
+    borderRadius: 15,
+    zIndex: 2,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    alignSelf: "center",
+  },
+  email: {
+    marginTop: 20,
+    fontFamily: "Lato-Bold",
+    fontSize: 20,
+    marginBottom: 10,
+    marginRight: "auto",
+  },
+  text: {
+    marginTop: 20,
+    fontFamily: "Lato-Bold",
+    fontSize: 20,
+    marginBottom: 10,
+    marginRight: "auto",
+  },
+  subtitle: {
+    fontFamily: "Lato-Regular",
+    fontSize: 15,
+    marginBottom: 10,
+    marginRight: "auto",
+  },
 
+  container: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderColor: "#f2f2f2",
+  },
+  footerText: {
+    bottom: 15,
+    fontFamily: "Lato-Regular",
+    fontSize: 12,
+    position: "absolute",
+  },
 
-    translucentBox: {
-        height: '50%',
-        width: '85%',
-        position: 'absolute',
-        top: 320,
-        borderRadius: 15,
-        zIndex: 2,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        alignSelf: 'center'
-    },
-    email: {
-        marginTop: 20,
-        fontFamily: 'Lato-Bold',
-        fontSize: 20,
-        marginBottom: 10,
-        marginRight: 'auto'
-
-    },
-    text: {
-        marginTop: 20,
-        fontFamily: 'Lato-Bold',
-        fontSize: 20,
-        marginBottom: 10,
-        marginRight: 'auto'
-
-    },
-    subtitle: {
-        fontFamily: 'Lato-Regular',
-        fontSize: 15,
-        marginBottom: 10,
-        marginRight: 'auto'
-
-    },
-
-    container: {
-        flex: 1,
-        backgroundColor: '#f2f2f2',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        borderColor: '#f2f2f2',
-    },
-    footerText: {
-        bottom: 15,
-        fontFamily: 'Lato-Regular',
-        fontSize: 12,
-        position: 'absolute'
-    },
-
-    viewTicketsButton: {
-        marginTop: 50,
-        backgroundColor: '#B731D9',
-        borderWidth: 5,
-        borderColor: '#fff'
-    },
-
+  viewTicketsButton: {
+    marginTop: 50,
+    backgroundColor: "#B731D9",
+    borderWidth: 5,
+    borderColor: "#fff",
+  },
 });
-
