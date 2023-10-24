7da8a8268a87bd81c0a3bdd5ef89471434fccc7b
Wei Jie <limweijie63@gmail.com>
2023-10-24 20:32:35 +0800

    feat/add scroll view for ticket page/weijie

--- Tixar-Client/src/screens/user/ticketsPage.js
+++ Tixar-Client/src/screens/user/ticketsPage.js
@@ -1,83 +1,77 @@
 import {
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    Image,
-    Pressable,
-  } from "react-native";
-  import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Pressable,
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-  import SearchField from '../../components/browseConcert/searchField';
-  import TicketCard from '../../components/userTickets/ticketCard';
-  import FilterButton from '../../components/userTickets/filterButton';
+import SearchField from "../../components/browseConcert/searchField";
+import TicketCard from "../../components/userTickets/ticketCard";
 
-  import { ColorContext } from "../../../context";
-  import { useContext } from "react"; 
+import { ColorContext } from "../../../context";
+import { useContext } from "react";
+import { ScrollView } from "react-native-gesture-handler";
 
-  
-  export default TicketsPage = ({ route, navigation }) => {
+export default TicketsPage = ({ route, navigation }) => {
+  const { colors } = useContext(ColorContext);
 
-    const {colors} = useContext(ColorContext);
+  // Function to handle the press event of the first ticket
+  const firstTicketPress = () => {
+    // console.log("first ticket pressed");
+    navigation.navigate("viewConcertPage"); //change this to haris's view ticket page
+  };
 
-    // Function to handle the press event of the first ticket
-    const firstTicketPress = () => {
-        // console.log("first ticket pressed");
-        navigation.navigate("viewConcertPage"); //change this to haris's view ticket page
-    };
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "flex-start",
+      backgroundColor: colors.background,
+    },
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            justifyContent: 'flex-start',
-            backgroundColor: colors.background,
-        },
-    
-        searchContainer: {
-            backgroundColor: colors.background,
-            marginVertical: 20,
-        },
-        ticketContainer: {
-            flex: 1, //grow and take up the space of the parent container
-            justifyContent: 'flex-start',
-            flexDirection: 'column',
-            backgroundColor: colors.background,
-        }
-    });
-  
-    return (
+    searchContainer: {
+      backgroundColor: colors.background,
+      marginVertical: 20,
+    },
+    ticketContainer: {
+      flex: 1, //grow and take up the space of the parent container
+      justifyContent: "flex-start",
+      flexDirection: "column",
+      backgroundColor: colors.background,
+    },
+  });
 
-        // container to contain all the elements
-        <View style={styles.container}>
+  return (
+    <View style={styles.container}>
+      {/* container for search bar and filter button */}
+      <View style={styles.searchContainer}>
+        {/* search field imported from browseConcert components */}
+        <SearchField />
+      </View>
 
-            {/* container for search bar and filter button */}
-            <View style={styles.searchContainer}>
-                
-                {/* search field imported from browseConcert components */}
-                <SearchField />
-                
-            </View>        
+      <ScrollView>
+        {/* container for the ticket cards */}
+        <View style={styles.ticketContainer}>
+          {/* first concert ticket*/}
+          <TicketCard
+            concertName={"Coldplay Concert"}
+            concertCategory={"Category: 1"}
+            concertReference={"Reference No. 123456789"}
+            onPress={() => {
+              firstTicketPress();
+            }}
+          />
 
-            {/* container for the ticket cards */}
-            <View style={styles.ticketContainer}>
-
-                {/* first concert ticket*/}
-                <TicketCard 
-                    concertName={'Coldplay Concert'}
-                    concertCategory={'Category: 1'}
-                    concertReference={'Reference No. 123456789'}
-                    onPress={() => {firstTicketPress()}}
-                />
-                
-                {/* second concert ticket*/}
-                <TicketCard 
-                    concertName={'Coldplay Concert'}
-                    concertCategory={'Category: 1'}
-                    concertReference={'Reference No. 123456789'}
-                />
-            </View>
-
+          {/* second concert ticket*/}
+          <TicketCard
+            concertName={"Coldplay Concert"}
+            concertCategory={"Category: 1"}
+            concertReference={"Reference No. 123456789"}
+          />
         </View>
-    );
+      </ScrollView>
+    </View>
+  );
 };
-  
