3d298fd06c1559019cc14e08ad08facabfd368f8
kloe <ongweitingchloe@gmail.com>
2023-11-05 16:20:30 +0800

    Format concert date/chloe

--- Tixar-Client/src/components/viewConcert/concertBlock.js
+++ Tixar-Client/src/components/viewConcert/concertBlock.js
@@ -1,99 +1,118 @@
-import { View, Image, Text, Pressable, StyleSheet } from 'react-native'
+import { View, Image, Text, Pressable, StyleSheet } from "react-native";
+// const formatDateString = (startDate, endDate) => {
+//   const startDay = startDate.getDate();
+//   const endDay = endDate.getDate();
+//   const month = startDate.toLocaleString("default", { month: "long" });
+//   const year = startDate.getFullYear();
 
-export default ConcertBlock = ({ concertName, venueName, dateText, imageBackground,
-    artistName, artistDescription, artistIcon, onPressFunction }) => {
-    return (
-        <Pressable style={styles.container}
-            onPress={() => {
-                onPressFunction
-                    ? onPressFunction()
-                    : console.log(concertName + ' pressed');
-            }}>
-            <Image source={imageBackground}
-                style={styles.imageBackground} />
-            <View style={{ paddingHorizontal: 15, }}>
-                <View style={{ height: 20 }} />
+//   if (startDay === endDay) {
+//     return `${startDay} ${month} ${year}`;
+//   } else {
+//     const days = [];
+//     for (let day = startDay; day <= endDay; day++) {
+//       days.push(day);
+//     }
+//     return `${days.join(", ")} ${month} ${year}`;
+//   }
+// };
 
-                <Text style={styles.title}>
-                    {concertName}
-                </Text>
+export default ConcertBlock = ({
+  concertName,
+  venueName,
+  startDate,
+  endDate,
+  imageBackground,
+  artistName,
+  artistDescription,
+  artistIcon,
+  onPressFunction,
+}) => {
+  //   const formattedDate = formatDateString(startDate, endDate);
+  return (
+    <Pressable
+      style={styles.container}
+      onPress={() => {
+        onPressFunction
+          ? onPressFunction()
+          : console.log(concertName + " pressed");
+      }}
+    >
+      <Image source={imageBackground} style={styles.imageBackground} />
+      <View style={{ paddingHorizontal: 15 }}>
+        <View style={{ height: 20 }} />
 
-                <View style={{ height: 30 }} />
+        <Text style={styles.title}>{concertName}</Text>
 
-                <Text style={styles.subtitle}>
-                    {venueName}
-                </Text>
+        <View style={{ height: 30 }} />
 
-                <View style={{ height: 7 }} />
+        <Text style={styles.subtitle}>{venueName}</Text>
 
-                <Text style={styles.subtitle}>
-                    {dateText}
-                </Text>
+        <View style={{ height: 7 }} />
 
-                <View style={{ height: 45 }} />
+        <Text style={styles.subtitle}>
+          {startDate === endDate ? startDate : `${startDate} to ${endDate}`}
+          {/* {formattedDate} */}
+        </Text>
 
-                <View style={styles.artistBlock}>
-                    <Image source={artistIcon}
-                        style={styles.artistIcon} />
-                    <View style={styles.artistTextContainer}>
-                        <Text style={styles.artistName}>
-                            {artistName}
-                        </Text>
-                        <View style={{ height: 5, }} />
-                        <Text style={styles.artistDescription}>
-                            {artistDescription}
-                        </Text>
-                    </View>
-                </View>
-            </View>
-        </Pressable>
-    );
-}
+        <View style={{ height: 45 }} />
+
+        <View style={styles.artistBlock}>
+          <Image source={artistIcon} style={styles.artistIcon} />
+          <View style={styles.artistTextContainer}>
+            <Text style={styles.artistName}>{artistName}</Text>
+            <View style={{ height: 5 }} />
+            <Text style={styles.artistDescription}>{artistDescription}</Text>
+          </View>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'green',
-        height: 240,
-        borderRadius: 20,
-        marginVertical: 13,
-        paddingVertical: 15,
-        overflow: 'hidden',
-    },
-    title: {
-        fontFamily: 'Lato-Bold',
-        fontSize: 25,
-        color: 'white',
-    },
-    subtitle: {
-        fontFamily: 'Lato-Regular',
-        fontSize: 15,
-        color: 'white',
-    },
-    artistIcon: {
-        height: 45,
-        width: 45,
-        resizeMode: 'contain',
-        borderRadius: 6,
-    },
-    imageBackground: {
-        height: 240,
-        width: '100%',
-        position: 'absolute',
-        alignSelf: 'center',
-        resizeMode: 'stretch',
-    },
-    artistBlock: {
-        flexDirection: 'row'
-    },
-    artistTextContainer: {
-        paddingHorizontal: 15,
-    },
-    artistName: {
-        fontFamily: 'Lato-Bold',
-        color: 'white',
-    },
-    artistDescription: {
-        fontFamily: 'Lato-Regular',
-        color: 'white',
-    }
+  container: {
+    backgroundColor: "green",
+    height: 240,
+    borderRadius: 20,
+    marginVertical: 13,
+    paddingVertical: 15,
+    overflow: "hidden",
+  },
+  title: {
+    fontFamily: "Lato-Bold",
+    fontSize: 25,
+    color: "white",
+  },
+  subtitle: {
+    fontFamily: "Lato-Regular",
+    fontSize: 15,
+    color: "white",
+  },
+  artistIcon: {
+    height: 45,
+    width: 45,
+    resizeMode: "contain",
+    borderRadius: 6,
+  },
+  imageBackground: {
+    height: 240,
+    width: "100%",
+    position: "absolute",
+    alignSelf: "center",
+    resizeMode: "stretch",
+  },
+  artistBlock: {
+    flexDirection: "row",
+  },
+  artistTextContainer: {
+    paddingHorizontal: 15,
+  },
+  artistName: {
+    fontFamily: "Lato-Bold",
+    color: "white",
+  },
+  artistDescription: {
+    fontFamily: "Lato-Regular",
+    color: "white",
+  },
 });
