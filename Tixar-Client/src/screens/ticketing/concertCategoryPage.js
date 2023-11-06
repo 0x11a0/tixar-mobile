d5c096c103cb5bfa068e47c7afefdf37a2252c78
kloe <ongweitingchloe@gmail.com>
2023-11-06 19:40:38 +0800

    select concert ticket information feature/chloe

--- Tixar-Client/src/screens/ticketing/concertCategoryPage.js
+++ Tixar-Client/src/screens/ticketing/concertCategoryPage.js
@@ -77,82 +77,56 @@ export default ConcertCategoryPage = ({ route, navigation }) => {
   if (!anySalesRoundMatchesConditions) {
     showAlertAndNavigateBack();
   } else {
-    console.log("========================");
-    console.log(filteredSalesRound);
-
     const quantityForRound = filteredSalesRound[0].allocation;
-    console.log(quantityForRound);
     const sessions = concert.sessions;
     const startDate = concert.sessions[0].start;
     const length = concert.sessions.length;
     const endDate = concert.sessions[length - 1].end;
-    console.log(startDate);
-    console.log(endDate);
     dateList = generateDateList(moment(startDate), moment(endDate));
-    console.log(dateList);
     const prices = filteredSalesRound[0].prices;
-    console.log("check the prices " + prices);
     categoryAndPrice = generateCategoryAndPrice(prices);
-    console.log("here is the title " + filteredSalesRound[0].title);
-    console.log("here are the prices " + filteredSalesRound[0].prices[0].price);
-    console.log(categoryAndPrice);
   }
-
-  console.log("-----------------");
-  console.log(concert.sessions);
 
   //for dates - get the date from the session, the first array element and the last one
   const { colors } = useContext(ColorContext);
   // STATE VARIABLES to store the text input from the user for quantity and category
   const [isButtonEnabled, setIsButtonEnabled] = useState(false);
 
-  const quantities = ["1", "2", "3", "4", "5"];
+  const quantities = ["1", "2", "3", "4"];
 
-  const categories = ["A", "B", "C", "D"];
-
-  //   const [date, setDate] = useState("Date");
-  //   const [quantity, setQuantity] = useState("Quantity");
-  //   const [category, setCategory] = useState("Category");
   const [date, setDate] = useState(null);
-  const [quantity, setQuantity] = useState(null);
+  const [quantity, setQuantity] = useState(quantities);
   const [category, setCategory] = useState(null);
 
   useEffect(() => {
     setIsButtonEnabled(date !== null && quantity !== null && category !== null);
   }, [date, quantity, category]);
 
-  const 
-
   const getQuantity = (selectedCategory, selectedDate) => {
-    // Filter sessions based on the selected date
-    const selectedDateSessions = concert.sessions.filter((session) => {
-      const sessionDate = moment(session.start).format("D MMM YYYY");
-      if (sessionDate == selectedDate) {
-        console.log("look at this " + session.capacity[0].available);
-        const capacity = session.capacity;
-        console.log(capacity);
-      }
-      return sessionDate === selectedDate;
-    });
-
-    // Find the session with the matching category
-    const matchingSession = selectedDateSessions.find((session) => {
-      return session.capacity.some((capacityEntry) => {
-        return capacityEntry.category === selectedCategory;
+    if (selectedCategory != null && selectedDate != null) {
+      const selectedDateSessions = concert.sessions.filter((session) => {
+        const sessionDate = moment(session.start).format("D MMM YYYY");
+        if (sessionDate === selectedDate) {
+          const capacity = session.capacity;
+          const selectedCategoryFiltered = selectedCategory
+            .split("-")[0]
+            .trim();
+          const selectedCapacity = capacity.find(
+            (individualCapacity) =>
+              individualCapacity.category === selectedCategoryFiltered
+          );
+          if (selectedCapacity) {
+            console.log(selectedCapacity.available);
+            return selectedCapacity.available;
+          }
+        }
       });
-    });
-
-    if (matchingSession) {
-      // If a matching session is found, return the available quantity for the selected category
-      const matchingCapacityEntry = matchingSession.capacity.find(
-        (capacityEntry) => capacityEntry.category === selectedCategory
-      );
-
-      return matchingCapacityEntry.quantity;
-    } else {
-      // Category not found for the selected date
-      return "N/A";
+      if (selectedDateSessions.length > 0) {
+        // assuming all sessions have the same available quantity, you can take the first one
+        return selectedDateSessions[0].capacity[0].available;
+      }
     }
+    return null;
   };
 
   const styles = StyleSheet.create({
@@ -226,64 +200,31 @@ export default ConcertCategoryPage = ({ route, navigation }) => {
         >
           {/* date picker that works on both iOS and Android */}
           <Text style={styles.subtitle}>Date</Text>
-          {/* <View>
-                        <DatePicker
-                            icon={require("../../assets/soft-ui-pro-react-native-v1.1.1/calendar3x.png")}
-                            minDate={new Date()}
-                        // maxDate={new Date(2024, 0, 27)}
-                        />
-                    </View> */}
-
-          {/* <OptionFields
-            optionText={date}
-            setOption={setDate}
-            antIconName="calendar"
-            options={date}
-          /> */}
           <View>
             <Picker
               selectedValue={date}
               onValueChange={(itemValue) => setDate(itemValue)}
             >
-              {dateList.map((date, index) => (
-                <Picker.Item key={index} label={date} value={date} />
+              {dateList.map((date) => (
+                <Picker.Item key={date} label={date} value={date} />
               ))}
             </Picker>
           </View>
 
           {/* seat category selection */}
           <Text style={styles.subtitle}>Seat Category</Text>
-          {/* <OptionFields
-                        optionText={"Category 1"}
-                        icon={require("../../assets/soft-ui-pro-react-native-v1.1.1/components3x.png")}
-                        onPressFunction={() => {
-                            console.log("category option clicked");
-                        }}
-                        onChangeTextFunction={(text) => {
-                            handleCategoryChange(text);
-                        }}
-                        keyboardType={"numeric"}
-                    /> */}
-          {/* <OptionFields
-            optionText={category}
-            setOption={setCategory}
-            antIconName={"tag"}
-            options={categories}
-          /> */}
           <View>
             <Picker
               selectedValue={category}
               onValueChange={(itemValue) => setCategory(itemValue)}
             >
-              {categoryAndPrice.map((category, index) => (
-                <Picker.Item key={index} label={category} value={category} />
+              {categoryAndPrice.map((category) => (
+                <Picker.Item key={category} label={category} value={category} />
               ))}
             </Picker>
           </View>
 
           <Text style={{ color: "gray", fontSize: 12, marginTop: 5 }}>
-            {/* Selected Category: {category} 
-            Selected Date: {date} */}
             Available Quantity: {getQuantity(category, date)}
           </Text>
 
@@ -300,12 +241,12 @@ export default ConcertCategoryPage = ({ route, navigation }) => {
                         }}
                         keyboardType={"numeric"}
                     /> */}
-          {/* <OptionFields
+          <OptionFields
             optionText={quantity}
             setOption={setQuantity}
             materialIconName={"format-list-numbered"}
             options={quantities}
-          /> */}
+          />
 
           {/* button that brings you to the purchase confirmation page */}
 
