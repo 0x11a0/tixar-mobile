24f5b9bb7cf0b75a98acb6a733be2ffea6ccf6bf
kloe <ongweitingchloe@gmail.com>
2023-11-07 09:24:58 +0800

    Integrated get all tickets/chloe

--- Tixar-Client/src/screens/user/ticketsPage.js
+++ Tixar-Client/src/screens/user/ticketsPage.js
@@ -1,18 +1,40 @@
-import {
-  View,
-  StyleSheet,
-} from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import SearchField from "../../components/browseConcert/searchField";
 import TicketCard from "../../components/userTickets/ticketCard";
 
 import { ColorContext } from "../../../context";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ScrollView } from "react-native-gesture-handler";
+import { AuthContext } from "../../../context";
 
 export default TicketsPage = ({ navigation }) => {
   const { colors } = useContext(ColorContext);
+  const { token } = useContext(AuthContext);
+  const [tickets, setTickets] = useState([]);
 
+  const getAllTickets = () => {
+    fetch("http://rt.tixar.sg:3000/api/ticket", {
+      method: "GET",
+      credentials: "include",
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTickets(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    navigation.addListener("focus", () => {
+      console.log("reloaded");
+      getAllTickets();
+    });
+  }, [navigation]);
 
   const styles = StyleSheet.create({
     container: {
@@ -44,25 +66,44 @@ export default TicketsPage = ({ navigation }) => {
       <ScrollView>
         {/* container for the ticket cards */}
         <View style={styles.ticketContainer}>
+          {tickets.map((ticket) => {
+            console.log(ticket);
+            console.log(ticket.type);
+            console.log(ticket._id);
+            const category = ticket.type;
+            const id = ticket._id;
+
+            return (
+              <TicketCard
+                key={id}
+                concertName={"Coldplay Concert"}
+                concertCategory={category}
+                concertReference={`Reference No. ${id}`}
+                onPress={() => {
+                  navigation.navigate("generatedUserTicketPage");
+                }}
+              />
+            );
+          })}
           {/* first concert ticket*/}
-          <TicketCard
+          {/* <TicketCard
             concertName={"Coldplay Concert"}
             concertCategory={"Category: 1"}
             concertReference={"Reference No. 123456789"}
             onPress={() => {
-                navigation.navigate("generatedUserTicketPage");
+              navigation.navigate("generatedUserTicketPage");
             }}
-          />
+          /> */}
 
           {/* second concert ticket*/}
-          <TicketCard
+          {/* <TicketCard
             concertName={"Coldplay Concert"}
             concertCategory={"Category: 1"}
             concertReference={"Reference No. 123456789"}
             onPress={() => {
-                navigation.navigate("generatedUserTicketPage");
+              navigation.navigate("generatedUserTicketPage");
             }}
-          />
+          /> */}
         </View>
       </ScrollView>
     </View>
