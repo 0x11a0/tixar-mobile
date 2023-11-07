80272be42e0f2e09111aa64c9c5b08bfc85ae31f
Wei Jie <limweijie63@gmail.com>
2023-11-07 13:12:02 +0800

    fixed edit profile page and styling

--- Tixar-Client/src/screens/user/editProfilePage.js
+++ Tixar-Client/src/screens/user/editProfilePage.js
@@ -3,21 +3,138 @@ import {
   View,
   Text,
   StyleSheet,
-  SafeAreaView,
   Image,
   Pressable,
   TextInput,
-  FlatList,
-  TouchableOpacity,
-  Button,
-  onPressLearnMore,
   Alert,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { AuthContext } from "../../../context";
+import { AuthContext, ColorContext } from "../../../context";
+import Button from "../../components/newApp/button";
 
 export default EditProfilePage = ({ route, navigation }) => {
+  const { colors } = useContext(ColorContext);
+
+  const styles = StyleSheet.create({
+    headerBox: {
+      position: "relative",
+      flex: 0.5,
+      alignItems: "center",
+      justifyContent: "flex-start",
+      zIndex: 1,
+    },
+    title: {
+      fontSize: 35,
+      fontFamily: "Lato-Bold",
+      color: "white",
+      marginTop: 40,
+    },
+    headerImage: {
+      marginTop: 30,
+      width: "95%",
+      height: 280,
+      borderRadius: 22,
+      resizeMode: "cover",
+      position: "absolute",
+    },
+
+    profileImage: {
+      flex: 0.5,
+      width: 100,
+      height: 100,
+      resizeMode: "cover",
+      marginTop: 8,
+    },
+
+    translucentBox: {
+      height: "75%",
+      width: "85%",
+      position: "absolute",
+      top: 130,
+      backgroundColor: colors.primary,
+      borderRadius: 15,
+      zIndex: 2,
+      alignItems: "center",
+      justifyContent: "flex-start",
+      alignSelf: "center",
+    },
+    subtitle: {
+      fontSize: 20,
+      fontFamily: "Lato-Bold",
+      color: colors.textPrimary,
+      marginTop: 24,
+    },
+
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      alignItems: "center",
+      justifyContent: "flex-start",
+    },
+    footerText: {
+      bottom: 15,
+      fontFamily: "Lato-Regular",
+      fontSize: 12,
+      position: "absolute",
+      color: colors.textDisabled,
+    },
+
+    fieldText: {
+      left: "5%",
+      fontSize: 18,
+      fontFamily: "Lato-Regular",
+      color: "#8F8F8F",
+    },
+
+    resetButton: {
+      width: "86%",
+      height: 35,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    resetBackgroundEnabled: {
+      marginTop: 55,
+      borderRadius: 8,
+      width: "86%",
+      height: 40,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    resetBackgroundDisabled: {
+      marginTop: 55,
+      borderRadius: 8,
+      width: "86%",
+      height: 40,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#E8ECEF",
+    },
+    resetTextEnabled: {
+      fontSize: 15,
+      fontFamily: "Lato-Bold",
+      color: "white",
+    },
+    resetTextDisabled: {
+      fontSize: 15,
+      fontFamily: "Lato-Bold",
+      color: "#252F40",
+    },
+    textBox: {
+      backgroundColor: colors.secondary,
+      borderRadius: 10,
+      padding: 10,
+      marginVertical: 10,
+      width: "86%",
+    },
+    buttonContainer: {
+      marginTop: 20,
+      alignItems: "center",
+      justifyContent: "center",
+      // backgroundColor: colors.accent,
+    },
+  });
+
   const [firstNameField, setFirstNameField] = useState("");
   const handleFirstName = (text) => {
     setFirstNameField(text);
@@ -48,6 +165,10 @@ export default EditProfilePage = ({ route, navigation }) => {
       Alert.alert("Invalid Email", "Please enter a valid email address.");
       return;
     }
+
+    // Show a loading indicator
+    setIsUpdating(true);
+
     fetch("http://rt.tixar.sg:3000/api/user", {
       method: "PUT",
       credentials: "include",
@@ -61,13 +182,17 @@ export default EditProfilePage = ({ route, navigation }) => {
         email: emailField === "" ? email : emailField,
       }),
     }).then((response) => {
+      setIsUpdating(false);
       if (response.ok) {
         console.log("Update successful");
+        navigation.navigate("userProfilePage");
       } else {
         console.log("Update unsuccessful");
       }
     });
   };
+
+  const [isUpdating, setIsUpdating] = useState(false); // Add a state variable to track the update process
 
   return (
     <KeyboardAwareScrollView
@@ -90,179 +215,53 @@ export default EditProfilePage = ({ route, navigation }) => {
           source={require("../../assets/profilepicture.png")}
           style={styles.profileImage}
         />
-        <View style={styles.fieldBox}>
+        <View style={styles.textBox}>
           <TextInput
             style={styles.fieldText}
             onChangeText={handleFirstName}
             value={firstNameField}
             placeholder="First name"
+            placeholderTextColor={colors.textDisabled}
+            color={colors.textPrimary}
           />
         </View>
-        <View style={styles.fieldBox}>
+        <View style={styles.textBox}>
           <TextInput
             style={styles.fieldText}
             onChangeText={handleLastName}
             value={lastNameField}
             placeholder="Last name"
+            placeholderTextColor={colors.textDisabled}
+            color={colors.textPrimary}
           />
         </View>
-        <View style={styles.fieldBox}>
+        <View style={styles.textBox}>
           <TextInput
             style={styles.fieldText}
             onChangeText={handleEmail}
             value={emailField}
             placeholder="Email"
+            placeholderTextColor={colors.textDisabled}
+            color={colors.textPrimary}
           />
         </View>
-        <ResetButton
-          isValid={
-            firstNameField !== "" || lastNameField !== "" || emailField !== ""
-          }
-          onPressFunction={() => {
-            handleUpdate();
-            navigation.pop();
-          }}
-        />
+
+        <View style={styles.buttonContainer}>
+          <Button
+            buttonText={isUpdating ? "UPDATING..." : "UPDATE"}
+            enableCondition={
+              firstNameField !== "" || lastNameField !== "" || emailField !== ""
+            }
+            onPressFunction={() => {
+              handleUpdate();
+            }}
+          />
+        </View>
+        <Text style={styles.footerText}>TIXAR</Text>
+
       </View>
-      <Text style={styles.footerText}>TIXAR</Text>
     </KeyboardAwareScrollView>
   );
 };
 
-const ResetButton = ({ isValid, onPressFunction }) => {
-  return (
-    <LinearGradient
-      colors={isValid ? ["#FF0080", "#7928CA"] : ["#E8ECEF", "#E8ECEF"]}
-      style={styles.resetBackgroundEnabled}
-      start={[0, 0]}
-      end={[1, 0]}
-    >
-      <Pressable
-        style={styles.resetButton}
-        onPress={() => {
-          onPressFunction();
-        }}
-      >
-        <Text
-          style={isValid ? styles.resetTextEnabled : styles.resetTextDisabled}
-        >
-          Update Profile
-        </Text>
-      </Pressable>
-    </LinearGradient>
-  );
-};
-const styles = StyleSheet.create({
-  headerBox: {
-    position: "relative",
-    flex: 0.5,
-    alignItems: "center",
-    justifyContent: "flex-start",
-    zIndex: 1,
-  },
-  title: {
-    fontSize: 35,
-    fontFamily: "Lato-Bold",
-    color: "white",
-    marginTop: 40,
-  },
-  headerImage: {
-    marginTop: 30,
-    width: "95%",
-    height: 280,
-    borderRadius: 22,
-    resizeMode: "cover",
-    position: "absolute",
-  },
 
-  profileImage: {
-    flex: 0.5,
-    width: 100,
-    height: 100,
-    resizeMode: "cover",
-    marginTop: 8,
-  },
-
-  translucentBox: {
-    height: "75%",
-    width: "85%",
-    position: "absolute",
-    top: 130,
-    backgroundColor: "rgba(255, 255, 255, 0.95)",
-    borderRadius: 15,
-    zIndex: 2,
-    alignItems: "center",
-    justifyContent: "flex-start",
-    alignSelf: "center",
-  },
-  subtitle: {
-    fontSize: 20,
-    fontFamily: "Lato-Bold",
-    color: "#394051",
-    marginTop: 24,
-  },
-
-  container: {
-    flex: 1,
-    backgroundColor: "#f2f2f2",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    borderColor: "#f2f2f2",
-  },
-  footerText: {
-    bottom: 15,
-    fontFamily: "Lato-Regular",
-    fontSize: 12,
-    position: "absolute",
-  },
-
-  fieldBox: {
-    height: 56,
-    width: "86%",
-    borderRadius: 10,
-    borderColor: "#D2D6DA",
-    borderWidth: 1,
-    justifyContent: "center",
-    marginTop: 10,
-  },
-  fieldText: {
-    left: "5%",
-    fontSize: 18,
-    fontFamily: "Lato-Regular",
-    color: "#8F8F8F",
-  },
-
-  resetButton: {
-    width: "86%",
-    height: 35,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  resetBackgroundEnabled: {
-    marginTop: 55,
-    borderRadius: 8,
-    width: "86%",
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  resetBackgroundDisabled: {
-    marginTop: 55,
-    borderRadius: 8,
-    width: "86%",
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#E8ECEF",
-  },
-  resetTextEnabled: {
-    fontSize: 15,
-    fontFamily: "Lato-Bold",
-    color: "white",
-  },
-  resetTextDisabled: {
-    fontSize: 15,
-    fontFamily: "Lato-Bold",
-    color: "#252F40",
-  },
-});
